b8ad4e0b5e9556a1fd614cdfb0f5eeccf66fa4ef
Yin, Jiayi <yinjiayi5@gmail.com>
2021-10-07 13:12:39 -0400

    Created current weather component card

---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import { Card } from 'semantic-ui-react' //Semantic UI is a library to design the interface
+
+const CardExampleCard = ({weatherData}) => (  //child component of app.js
+  <Card>
+    <Card.Content>
+        <Card.Header className="header">{weatherData.name}</Card.Header>
+    </Card.Content>
+  </Card>
+)
+
+export default CardExampleCard;

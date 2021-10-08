b3d8ad07000e7d9a7068b0b3e03bf06479ba8df0
daviddcho <amazingdcho@gmail.com>
2021-10-07 20:05:40 -0400

    added location search component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import React, {useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  const [currentTemp, setCurrentTemp] = useState(0); 
+class LocationSearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '', // This might be useful for autocomplete later?
+    }
+    this.handleInputChange= this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  useEffect(() => {
-    fetch('/api/currently').then(res => res.json()).then(data => {
-      setCurrentTemp(data.currentTemp);
-    });
-  }, []);
+  handleInputChange(e) {
+    this.setState({userInput: e.target.value});
+    console.log("User input: ", e.target.value);
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <b>Better Weather</b>
-        <p>The current temperature is {currentTemp}</p>
-      </header>
-    </div>
-  );
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onLocationSubmit(this.state.userInput);
+  }
+
+  render() {
+    const userInput = this.state.userInput;
+    return ( 
+      <div>
+        <form onSubmit={this.handleSubmit}> 
+          <input 
+            type="text"
+            value={userInput} 
+            onChange={this.handleInputChange} />
+          <input type="submit"/>
+        </form>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: 'New York',
+    };
+    this.handleLocationSubmit = this.handleLocationSubmit.bind(this);
+  }
+
+  handleLocationSubmit(location) {
+    this.setState({location: location});
+    console.log("Submitted location: ", location);
+  }
+    
+  render() {
+    const location = this.state.location; 
+    return (
+      <div> 
+        <LocationSearchBar
+          location={location}
+          onLocationSubmit={this.handleLocationSubmit} />
+        <p> Weather for: {location} </p>
+      </div>
+    );
+  }
 }
 
 export default App;

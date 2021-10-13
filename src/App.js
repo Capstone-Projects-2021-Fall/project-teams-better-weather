8f2a8ae877d3ccfe32e0b441a1003def88be5d38
Yin, Jiayi <yinjiayi5@gmail.com>
2021-10-13 07:56:59 -0400

    Added images and search to main page
    Also set up the API from OpenWeather

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import React, {useState, useEffect } from 'react';
-import './style/App.css';
+import React from 'react';
+
 import Weather from './components/Weather';
 
+const api = {
+    key: "2292aca2fd8419dfc5d684db85f58a36",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
-  const [currentTemp, setCurrentTemp] = useState(0);
+  //const [currentTemp, setCurrentTemp] = useState(0);
   
   //To be used later
-  const [cityName, setCityName] = useState("Philadelphia")
+  /*const [cityName, setCityName] = useState("Philadelphia")
   const [rain, setRain] = useState(3)
-  const [wind, setWind] = useState(4)
+  const [wind, setWind] = useState(4)*/
 
+  /*
   useEffect(() => {
     fetch('/api/currently').then(res => res.json()).then(data => {
       setCurrentTemp(data.currentTemp);
     });
   }, []);
+  */
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <b>Better Weather</b>
-        {(typeof currentTemp != 'undefined') ? (
-            <Weather weatherData={currentTemp}/>
-          ):(
-            <div></div>
-      )}
-      </header>
-    </div>
+      <div className='App-header'>
+        <h1>Better Weather</h1>
+
+        <Weather />
+      </div>
   );
 }
 
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import '../style/Weather.css';
-import { Card } from 'semantic-ui-react' //Semantic UI is a library to design the interface
+//import { Card } from 'semantic-ui-react' //Semantic UI is a library to design the interface
 
 //<p className="weather-info">Temperature{weatherData.name}</p>  //needed when implenting with API
 
+const api = {
+  key: "2292aca2fd8419dfc5d684db85f58a36",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function currentWeather(){
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+          />
+        </div>
+      </main>
+    </div>
+  );
+  
+}
+
+export default currentWeather;
+
+/*
 const CardExampleCard = ({weatherData, location}) => (  //child component of app.js
   <Card>
     <Card.Content>
@@ -16,3 +41,4 @@ const CardExampleCard = ({weatherData, location}) => (  //child component of app
 )
 
 export default CardExampleCard;
+*/

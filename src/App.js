6ad1e0b1e83609e8d339e04012435809d46a3ef6
Yin, Jiayi <yinjiayi5@gmail.com>
2021-10-15 12:38:08 -0400

    Made current weather as a component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-import CurrentDate from './components/CurrentDate';
-import './style/Weather.css';  //change into App.css
-import sun from './icons/sun/26.png';  //Make component to change icons based on weather.weather[0]
+import Weather from './components/weather.js';
 
-const api = {
-  key: "2292aca2fd8419dfc5d684db85f58a36",
-  base: "https://api.openweathermap.org/data/2.5/"  //for search
-}
 
 function App() {
   //const [currentTemp, setCurrentTemp] = useState(0);
@@ -25,67 +19,10 @@ function App() {
   }, []);
   */
 
-  //All from Open Weather API
-  const [query, setQuery] = useState('');  //The city name
-  const [weather, setWeather] = useState({});  //Current weather temp info
-
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then(res => res.json())
-      .then(result => {
-        setWeather(result);
-        setQuery('');
-        console.log(weather);
-      });
-    }
-  }
 
   return (
   <div className='App-header'>
-
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app')
-      : 'app'}>
-      <main>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search"
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {(typeof weather.main != "undefined") ? (
-          <div>
-            <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
-                <div className="date">
-                  <CurrentDate />
-                </div>                
-            </div>
-            <div className="weather-icon-box">
-              <div className="icons">
-                <img src={sun} height={250} width={250} alt='Sun icon'/>
-              </div>
-            </div>
-            <div className="weather-box">              
-              <div className="temp">
-                  {Math.round((weather.main.temp) * (9/5) + 32) }°F
-                  <div className="weather">{weather.weather[0].main}</div>                  
-              </div>
-            </div>
-          </div>
-        ) : ('')}
-
-        <div className="hourly-temp-box">
-          <div className="time-frame">
-            *Hourly Temps Here*
-          </div>
-        </div>
-      </main>
-    </div>
+    <Weather />
   </div>
   );
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,12 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CurrentDate from './CurrentDate.js'
+import '../style/Weather.css';
+import sun from '../icons/sun/26.png';  //Make component to change icons based on weather.weather[0] (weather type)
+
 //import { Card } from 'semantic-ui-react' //Semantic UI is a library to design the interface
 
 //<p className="weather-info">Temperature{weatherData.name}</p>  //needed when implenting with API
 
-function currentWeather(props){
+const api = {
+  key: "2292aca2fd8419dfc5d684db85f58a36",
+  base: "https://api.openweathermap.org/data/2.5/"  //for search
+}
+
+function CurrentWeather(){
+
+  //All from Open Weather API
+  const [query, setQuery] = useState('');  //The city name
+  const [weather, setWeather] = useState({});  //Current weather temp info
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(weather);
+      });
+    }
+  }
   
-  return <div>hello</div>
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app')
+      : 'app'}>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+                <div className="date">
+                  <CurrentDate />
+                </div>                
+            </div>
+            <div className="weather-icon-box">
+              <div className="icons">
+                <img src={sun} height={250} width={250} alt='Sun icon'/>
+              </div>
+            </div>
+            <div className="weather-box">              
+              <div className="temp">
+                  {Math.round((weather.main.temp) * (9/5) + 32) }°F
+                  <div className="weather">{weather.weather[0].main}</div>                  
+              </div>
+            </div>
+          </div>
+        ) : ('')}
+
+        <div className="hourly-temp-box">
+          <div className="time-frame">
+            *Hourly Temps Here*
+          </div>
+        </div>
+      </main>
+    </div>
+  );
   
 }
 
-export default currentWeather;
+export default CurrentWeather;

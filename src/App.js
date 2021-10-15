1a7c885df583608a457f3c1a7f057f38d114d707
Yin, Jiayi <yinjiayi5@gmail.com>
2021-10-14 23:16:42 -0400

    Updated temp to Fahrenheit

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
             </div>
             <div className="weather-box">              
               <div className="temp">
-                  {Math.round(weather.main.temp)}°C
+                  {Math.round((weather.main.temp) * (9/5) + 32) }°F
                   <div className="weather">{weather.weather[0].main}</div>                  
               </div>
             </div>

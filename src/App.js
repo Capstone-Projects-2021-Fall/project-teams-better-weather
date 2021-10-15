719ed58e8aba4ef4964b2d9ba10bfab7275591c4
Yin, Jiayi <yinjiayi5@gmail.com>
2021-10-14 20:12:37 -0400

    Switched Weather component into App
    Going to use components for saving/adding locations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import Weather from './components/Weather';
+import CurrentDate from './components/CurrentDate';
+import './style/Weather.css';  //change into App.css
 
+
+const api = {
+  key: "2292aca2fd8419dfc5d684db85f58a36",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   //const [currentTemp, setCurrentTemp] = useState(0);
@@ -19,12 +25,56 @@ function App() {
   }, []);
   */
 
-  return (
-      <div className='App-header'>
-        <h1>Better Weather</h1>
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
-        <Weather />
-      </div>
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(weather);
+      });
+    }
+  }
+
+  return (
+  <div className='App-header'>
+    <h1>Better Weather</h1>
+
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+                  <div className="date">
+                      <CurrentDate />
+                    </div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                  {Math.round(weather.main.temp)}°C
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ('')}
+      </main>
+    </div>
+  </div>
   );
 }
 
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,41 +1,11 @@
 import React from 'react';
-import '../style/Weather.css';
-import CurrentDate from './CurrentDate';
 //import { Card } from 'semantic-ui-react' //Semantic UI is a library to design the interface
 
 //<p className="weather-info">Temperature{weatherData.name}</p>  //needed when implenting with API
 
-const api = {
-  key: "2292aca2fd8419dfc5d684db85f58a36",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
-
-function currentWeather(){
-  return (
-    <div className="app">
-      <main>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search"
-          />
-        </div>
-        <div className="location-box">
-          <div className="location">Philadelphia</div>
-          <div className="date">
-            <CurrentDate />
-          </div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">
-            15°C
-          </div>
-          <div className="weather">Sunny</div>
-        </div>
-      </main>
-    </div>
-  );
+function currentWeather(props){
+  
+  return <div>hello</div>
   
 }
 

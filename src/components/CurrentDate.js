13e99e605e4c9889ff91666d676fe2e1eff7b009
Yin, Jiayi <yinjiayi5@gmail.com>
2021-10-16 20:12:43 -0400

    Fixed styling

--- src/components/CurrentDate.js
+++ src/components/CurrentDate.js
@@ -2,7 +2,7 @@ function CurrentDate() {
     var currDate = new Date();
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var date =  days[currDate.getDay()] + ' ' + months[currDate.getMonth()] + ' ' + currDate.getDate() + ',' + currDate.getFullYear();
+    var date =  days[currDate.getDay()] + ' ' + months[currDate.getMonth()] + ' ' + currDate.getDate() + ', ' + currDate.getFullYear();
     const newCurrDate = date;
 
     return(

2a4e274e29eb13c9196032628cd10a8593f3eeb1
Eriksiano Kapaj <eriksianobusiness@gmail.com>
2021-10-22 17:45:39 -0400

    Navbar implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LocationSearch from './components/LocationSearch';
 import CurrentWeather from './components/CurrentWeather.js';
 import HourlyWeather from './components/HourlyWeather.js';
 import './style/Weather.css';
+import * as ReactBootStrap from "react-bootstrap";
 
 const api = {
   key: "2292aca2fd8419dfc5d684db85f58a36", // exposed key!
@@ -54,6 +55,27 @@ function App() {
   }
 
   return (
+    <div>
+    <ReactBootStrap.Navbar bg="light" expand="lg">
+  <ReactBootStrap.Container>
+    <ReactBootStrap.Navbar.Brand href="#home">Better Weather</ReactBootStrap.Navbar.Brand>
+    <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
+      <ReactBootStrap.Nav className="me-auto">
+        <ReactBootStrap.Nav.Link href="#home">Home</ReactBootStrap.Nav.Link>
+        <ReactBootStrap.Nav.Link href="#link">Link</ReactBootStrap.Nav.Link>
+        <ReactBootStrap.NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
+          <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
+          <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
+          <ReactBootStrap.NavDropdown.Divider />
+          <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+        </ReactBootStrap.NavDropdown>
+      </ReactBootStrap.Nav>
+    </ReactBootStrap.Navbar.Collapse>
+  </ReactBootStrap.Container>
+</ReactBootStrap.Navbar>
+
     <div className={background()}>
       <div className="main">
           <LocationSearch 
@@ -64,6 +86,7 @@ function App() {
           <HourlyWeather 
             weather={hourly} />
       </div>
+    </div>
     </div>
   );
 }

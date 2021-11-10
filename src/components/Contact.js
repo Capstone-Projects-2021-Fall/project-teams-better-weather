7a6879c7aa32fb05685a800d71449f0cb398f474
daviddcho <amazingdcho@gmail.com>
2021-11-10 16:19:50 -0500

    add shorter

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,32 @@
 import React from 'react'
-import * as ReactBootStrap from "react-bootstrap";
+import {Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import {Route, Link} from 'react-router-dom';
 
 function Contact(){
   return(
     <div>
-  
-      <ReactBootStrap.Navbar bg="light" expand="lg">
-        <ReactBootStrap.Container>
-          <ReactBootStrap.Navbar.Brand href="#home">Better Weather</ReactBootStrap.Navbar.Brand>
-          <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
-            <ReactBootStrap.Nav className="me-auto">
-              <ReactBootStrap.Nav.Link href="#home">Home</ReactBootStrap.Nav.Link>
-              <ReactBootStrap.Nav.Link as={Link} to="/contact" >Contact</ReactBootStrap.Nav.Link>
-              <ReactBootStrap.NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Divider />
-                <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
-              </ReactBootStrap.NavDropdown>
-            </ReactBootStrap.Nav>
-          </ReactBootStrap.Navbar.Collapse>
-        </ReactBootStrap.Container>
-      </ReactBootStrap.Navbar>
-
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Better Weather</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link as={Link} to="/contact" >Contact</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div>
         <h1> Contact us</h1>
       </div>
-
     </div>
   );
 }

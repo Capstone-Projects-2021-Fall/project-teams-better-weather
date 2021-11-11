d05b9c9fc805a4381a18c9a0cfb87c2b66a99679
daviddcho <amazingdcho@gmail.com>
2021-11-11 18:07:14 -0500

    already in components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import Signup from "./components/Signup";
 import { AuthProvider } from "./contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MainPage from "./components/MainPage"
+import Signup from "./components/Signup";
 import Login from "./components/Login"
 import ForgotPassword from './components/ForgotPassword.js'
 
 function App() {
   return (
-      <div>
-        <Router> 
-          <AuthProvider>
-            <Switch>
-              <Route exact path="/" component={MainPage} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
+    <div>
+      <Router> 
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 }
 

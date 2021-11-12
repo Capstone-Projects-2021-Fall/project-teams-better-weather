fd8f6b8d10c7b0f145bf464f5bb42f1c422330b9
Yin, Jiayi <yinjiayi5@gmail.com>
2021-11-12 09:36:23 -0500

    Added some pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages';
 import Settings from './pages/settings';
+import Signup from './pages/signup';
+import About from './pages/about';
+import Signin from './pages/signin';
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
           <Route path="/settings" exact component={Settings} />
+          <Route path="/sign-up" exact component={Signup} />
+          <Route path="/sign-in" exact component={Signin} />
         </Switch>
       </Router>
     </div>
---
+++ src/pages/signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Signup(){
+    return (
+        <>
+            <div className="pages-bkg">
+                <Navbar />
+            </div>
+            <h1>Sign Up</h1>
+        </>
+    )
+}
+
+export default Signup;

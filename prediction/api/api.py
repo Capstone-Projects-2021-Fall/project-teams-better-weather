0ee037e5c63fff06d5ab4d11cdcb3fb2da876d25
daviddcho <amazingdcho@gmail.com>
2021-11-12 17:19:04 -0500

    oops not here

--- prediction/api/api.py
+++ prediction/api/api.py
@@ -16,25 +16,3 @@ def get_predictions():
   response.headers.add("Access-Control-Allow-Origin", "*")
   return response
 
-def get_prediction(coord):
-  """
-  Randomly generated predictions for now
-  """
-  data = {}    
-  data["lon"], data["lat"] = coord
-  summaries = ["cloudy", "mostly cloudy", "partly cloudy", "clear", "rain", "humid"]
-  data["hourly"] = {}
-  hours = []
-  for i in range(12):
-    x = {}
-    x["time"] = i 
-    x["summary"] = summaries[random.randint(0, len(summaries)-1)]
-    x["precipIntensity"] = round(random.uniform(0, 1), 2)
-    x["precipProbability"] = round(random.uniform(0, 1), 2)
-    x["temperature"] = round(random.uniform(0, 100), 2)
-    x["humidity"] = round(random.uniform(0, 1), 2)
-    hours.append(x)
-  data["hourly"]["data"] = hours
-  return json.dumps(data) 
-
-

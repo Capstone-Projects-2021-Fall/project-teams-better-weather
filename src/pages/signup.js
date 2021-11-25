d10a094f6d95da25c5064a8487951bbb25dffe71
daviddcho <amazingdcho@gmail.com>
2021-11-24 20:27:54 -0500

    no same names anymore

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Signup from "../components/Signup";
+import Register from "../components/Register";
 
 /**
  * Function that renders the signup page. This contains the navigation bar, signup component.
  * @component
  * @returns {JSX.Element} JSX render of Signup page
  */
-export default function SignUpPage() {
+export default function SignUp() {
   return (
     <>
       <div className="pages-bkg">
         <Navbar />
       </div>
-      <Signup />
+      <Register />
     </>
   );
 }

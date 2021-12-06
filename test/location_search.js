701ebf3cb3f49b7ff29c664969d632b7404d7e21
daviddcho <amazingdcho@gmail.com>
2021-12-05 19:08:44 -0500

    start unit tests

---
+++ test/location_search.js
@@ -0,0 +1,10 @@
+import {render} from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import * as React from "react"
+
+import LocationSearch from "src/components/LocationSearch"
+
+test("test test", () => {
+  const handleSubmit = jest.fn()
+
+})

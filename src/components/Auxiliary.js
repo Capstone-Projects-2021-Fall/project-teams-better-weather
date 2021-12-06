0a2faae87c6f3cac4b0540c6c3a59ae5d831e1d2
daviddcho <amazingdcho@gmail.com>
2021-12-05 20:38:29 -0500

    fix indent

--- src/components/Auxiliary.js
+++ src/components/Auxiliary.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "../style/Weather.css";
 
 /**
@@ -8,19 +7,19 @@ import "../style/Weather.css";
  * @param {object} hourly - List of 12 hours of weather information (temperature, summary, etc)
  * @returns {JSX.Element} JSX render of Hourly Weather
  */
- export default function AuxiliaryWeather({humidity, windGust, windSpeed, pressure  }) {
-  console.log("windGust", windGust)
-    return (
-      <div className="carousel-scroll">
-        <h3 className="hourly-title">Auxiliary Weather Conditions</h3>
-        <div className="scrollHours">
-       <div>
-         <div> Humidity: {humidity} </div>
+export default function AuxiliaryWeather({ humidity, windGust, windSpeed, pressure }) {
+  console.log("windGust", windGust);
+  return (
+    <div className="carousel-scroll">
+      <h3 className="hourly-title">Auxiliary Weather Conditions</h3>
+      <div className="scrollHours">
+        <div>
+          <div> Humidity: {humidity} </div>
           <div> Wind Gust: {windGust} </div>
           <div> Wind Speed: {windSpeed} </div>
           <div> Pressure: {pressure} </div>
-       </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
